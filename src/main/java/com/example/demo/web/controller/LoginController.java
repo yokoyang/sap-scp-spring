a43320513d002eeb02c4ca32c68b45f7639e0ddc
ryan Liu <ryan.liu@sap.com>
2017-08-11 15:56:18 +0800

    login to do

--- src/main/java/com/example/demo/web/controller/LoginController.java
+++ src/main/java/com/example/demo/web/controller/LoginController.java
@@ -6,22 +6,15 @@ import javax.net.ssl.SSLSession;
 
 import com.example.demo.web.Utils.SSLUtil;
 import com.example.demo.web.model.LoginInputViewModel;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 
 @RestController
 public class LoginController {
@@ -33,6 +26,8 @@ public class LoginController {
         final String url = "https://10.58.106.72:50000/b1s/v1/Login";
         SSLUtil.turnOffSslChecking();
         RestTemplate restTemplate = new RestTemplate();
+
+        // override to ignore the SSL certificate
         HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
             public boolean verify(String hostname, SSLSession session) {
                 return true;
@@ -43,6 +38,8 @@ public class LoginController {
         requestHeaders.add("Content-Type", "application/json");
 
         HttpEntity<LoginInputViewModel> request = new HttpEntity<LoginInputViewModel>(loginInput, requestHeaders);
+
+        //TODO failed login handler
         return restTemplate.postForObject(url, request, String.class);
 
     }
