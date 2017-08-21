0669898ac7559d1accffb932f70cba6aef7c0e79
ryan Liu <ryan.liu@sap.com>
2017-08-21 11:13:08 +0800

    Dozer with XML

--- src/test/java/com/example/demo/DozerTest.java
+++ src/test/java/com/example/demo/DozerTest.java
@@ -4,10 +4,16 @@ import org.dozer.DozerBeanMapper;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class DozerTest {
     private DozerBeanMapper mapper;
+
+    private void configureMapper(String... mappingFileUrls) {
+        mapper.setMappingFiles(Arrays.asList(mappingFileUrls));
+    }
 
     @Before
     public void before() throws Exception {
@@ -31,6 +37,27 @@ public class DozerTest {
 
         assertEquals(dest.getId(), 320);
         assertEquals(dest.getPoints(), 15);
+    }
+
+    @Test
+    public void givenSrcAndDestWithDifferentFieldNamesWithCustomMapper_whenMaps_thenCorrect() {
+        configureMapper("dozer_mapping.xml");
+        Personne frenchAppPerson = new Personne("Sylvester Stallone", "Rambo", 70);
+        Person englishAppPerson = mapper.map(frenchAppPerson, Person.class);
+        assertEquals(englishAppPerson.getName(), frenchAppPerson.getNom());
+        assertEquals(englishAppPerson.getNickname(), frenchAppPerson.getSurnom());
+        assertEquals(englishAppPerson.getAge(), frenchAppPerson.getAge());
+    }
+
+    @Test
+    public void givenSrcAndDest_whenMapsOnlySpecifiedFields_thenCorrect() {
+        configureMapper("dozer_mapping2.xml");
+        Person englishAppPerson = new Person("Shawn Corey Carter","Jay Z", 46);
+        Personne frenchAppPerson = mapper.map(englishAppPerson, Personne.class);
+
+        assertEquals(frenchAppPerson.getNom(), englishAppPerson.getName());
+        assertEquals(frenchAppPerson.getSurnom(),englishAppPerson.getNickname());
+        assertEquals(frenchAppPerson.getAge(), 0);
     }
 
 }
